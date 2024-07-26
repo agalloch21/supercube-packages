c90bd0f0867d372d558d5540273b844166f25f4b
agalloch21 <agalloch21@gmail.com>
2024-07-26 10:33:14 +0800

    update lava particle

--- xiaobo.supercube.interaction/Runtime/NormalMap_BlitSequence.cs
+++ xiaobo.supercube.interaction/Runtime/NormalMap_BlitSequence.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class NormalMap_BlitSequence : MonoBehaviour
 {
     public RenderTexture sourceRT;
+//    public RenderTexture blurredRT;
+//    public Material matBlur;
     public RenderTexture destinationRT;
     public Material matNormalMap;
 
     // Update is called once per frame
     void Update()
     {
+        //Graphics.Blit(sourceRT, blurredRT, matBlur, 0);
+
         Graphics.Blit(sourceRT, destinationRT, matNormalMap, 0);
     }
 }
